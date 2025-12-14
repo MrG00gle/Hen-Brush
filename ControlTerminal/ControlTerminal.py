89b28c5ce3de723da5f7b43a3bfda00016ed7625
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-12-14 20:40:41 +0100

    Added drone list update/create methods for terminal view.

--- ControlTerminal/ControlTerminal.py
+++ ControlTerminal/ControlTerminal.py
@@ -2,6 +2,8 @@ from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
 from textual.widgets import Header, Footer, Button, DataTable, RichLog, Label
 from textual.binding import Binding
+from textual.widgets._data_table import CellDoesNotExist
+
 from ControlTower import ControlTower
 
 
@@ -44,18 +46,20 @@ class ControlTerminal(App, ControlTower):
     """
 
     def __init__(self,
-                 # path_to_animation: str,
-                 # serial_port: str | None = None,
-                 # serial_speed: int | None = None,
-                 # serial_timeout: int | None = None,
+                 path_to_animation: str,
+                 serial_port: str | None = None,
+                 serial_speed: int | None = None,
+                 serial_timeout: int | None = None,
                  **kwargs):
 
         App.__init__(self, **kwargs)
-        # ControlTower.__init__(self,
-        #     path_to_animation=path_to_animation,
-        #     serial_port=serial_port,
-        #     serial_speed=serial_speed,
-        #     serial_timeout=serial_timeout)
+
+        ControlTower.__init__(self,
+            path_to_animation=path_to_animation,
+            serial_port=serial_port,
+            serial_speed=serial_speed,
+            serial_timeout=serial_timeout)
+
         self.left_width = 70
 
     def compose(self) -> ComposeResult:
@@ -65,13 +69,8 @@ class ControlTerminal(App, ControlTower):
         # Main content area
         with Horizontal(id="main-content"):
             # Left side: Scrollable list of drones
-            with Vertical(id="drones-section"):
-                yield Label("Scrollable List of Drones")
-                # table = DataTable()
-                # table.add_columns("ID", "Battery %", "Drone Status", "Animation Status", "Checkbox")
-                # # Add example rows (you can populate dynamically)
-                # table.add_row("1", "85%", "Idle", "Off", "[ ]")
-                # table.add_row("2", "92%", "Flying", "On", "[x]")
+            with Vertical(id="drone-section"):
+                yield Label("Drone List")
                 yield DataTable(name="DroneTable", id="drone_table", show_cursor=True, zebra_stripes=True)
 
             # Right side: Scrollable logs
@@ -92,17 +91,55 @@ class ControlTerminal(App, ControlTower):
         self.update_sizes()
 
         # ===== Drone Table =====
-        drone_table = self.query_one(selector="#drone_table", expect_type=DataTable)
-        drone_table.cursor_type = "row"
-        drone_table.add_columns("ID", "Battery %", "Drone Status", "Animation Status", "Checkbox")
-        drone_table.add_row("1", "85%", "Idle", "Off", "[ ]")
-        drone_table.add_row("2", "92%", "Flying", "On", "[x]")
+        self.create_drone_table()
 
         # Example: Write something to logs
         logs = self.query_one(RichLog)
         logs.write("Application started.")
         logs.write("Use Ctrl+, to decrease left pane, Ctrl+. to increase.")
 
+    def create_drone_table(self):
+        """Creates DroneTable/drone_table and populate rows"""
+        drone_table = self.query_one(selector="#drone_table", expect_type=DataTable)
+        drone_table.cursor_type = "row"
+        drone_table.add_columns("ID", "Drone Status", "Animation Status", "Position", "Pos. error", "Animation Step")
+
+        for drone in self.drones:
+            drone_table.add_row(
+                f"{self.drones.index(drone)}",
+                f"{drone.status.name}",
+                f"{drone.animation_status}",
+                f"{drone.reported_position}"
+                f"{drone.position_error}",
+                f"{drone.animation_step}/{drone.animation_len}",
+                key=f"{self.drones.index(drone)}"
+            )
+
+    def update_drone_table(self):
+        """Update DroneTable/drone_table"""
+        drone_table = self.query_one(selector="#drone_table", expect_type=DataTable)
+        for drone in self.drones:
+
+            value_list = [
+                str(self.drones.index(drone)),
+                drone.status.name,
+                str(drone.animation_status),
+                str(drone.reported_position),
+                str(drone.position_error),
+                str(drone.animation_step)
+            ]
+
+            i = 0
+            for col in drone_table.columns:
+                try:
+                    if i == 5:
+                        drone_table.update_cell(row_key=str(self.drones.index(drone)), column_key=col, value=f"{value_list[i]}/{drone.animation_len}")
+                    else:
+                        drone_table.update_cell(row_key=str(self.drones.index(drone)), column_key=col, value=value_list[i])
+                    i = + 1
+                except CellDoesNotExist:
+                    logs = self.query_one(RichLog)
+                    logs.write(f"Cell ({self.drones.index(drone)}/{col}) - does not exist!!!")
 
     def update_sizes(self) -> None:
         """Update the sizes of the panes."""
